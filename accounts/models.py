b5405f3acb241ccfb89ca8e3bc2eebeb65ba473f
DemoAM <abdulmateennmalik@gmail.com>
2024-06-19 14:29:09 +0500

    new

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser
 
 
-class Seller(User):
-    name = models.CharField(max_length=50)
-    bio = models.TextField(max_length=500)
+class Seller(AbstractBaseUser):
+    name = models.CharField(max_length=254)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=254)
+    user_type = models.CharField(max_length=15,choices={'seller':'Seller'},default="Seller")
+
+    def Is_seller(self):
+        return bool(self.user_type=='seller')
 
 
 
--- base/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from accounts.models import Seller
-class Book(models.Model):
-    author = models.ForeignKey(Seller,on_delete=models.SET_DEFAULT,default='Anonymous')
-    book_name = models.CharField(max_length=50)
-    price = models.IntegerField()
